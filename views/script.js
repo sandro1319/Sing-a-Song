8fdaa0a002cfb075ffaef54c86a00ac7791d26e8
Sandokan <sandro.ceh@gmail.com>
2025-10-20 17:52:36 +0200

    Popravljanje napak

--- views/script.js
+++ views/script.js
@@ -156,6 +156,8 @@ function getRandomWord() {
     'celoten', 'zakaj', 'veter', 'zima', 'želeti', 'ženska', 'beseda', 'delo', 'svet', 'letos',
     'Slovenija', 'svoje', 'nič', 'sam', 'vse', 'kot', 'dan', 'naj', 'spet', 'delati', 'zdaj', 'svet', 'treba', 'kar',
     'srce', 'sem', 'bom', 'kjer', 'sam', 'čas', 'življenje', 'smo', 'ustnice', 'več',
+    'Slovenija', 'svoje', 'nič', 'sam', 'vse', 'kot', 'dan', 'naj', 'spet', 'delati', 'zdaj', 'svet', 'treba', 'kar',
+    'srce', 'sem', 'bom', 'kjer', 'sam', 'čas', 'življenje', 'smo', 'umba', 'več',
     'vsi', 'vsak', 'med', 'moje', 'gor', 'pesem', 'pot', 'lepo', 'menoj', 'ljubezen',
     'nam', 'refren', 'brez', 'ljubezni', 'oba', 'morda', 'otok', 'nič', 'naprej', 'sera',
     'tam', 'kako', 'saj', 'tja', 'včasih', 'sonce', 'nikdar', 'sva', 'srca', 'čuk',
